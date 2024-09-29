4094bd0d64b055aa1849c8cdcd67583350e277f4
AchyutSingh <achyut.singh@reltio.com>
2024-09-29 10:31:11 +0530

    first commit

---
+++ src/main/java/org/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,23 @@
+package org.example.jenkinsdemo;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+    private static final Logger logger = Logger.getLogger(JenkinsDemoApplication.class.getName());
+
+    @PostConstruct
+    public void init(){
+        logger.info("init");
+    }
+
+    public static void main(String[] args) {
+        logger.info("main");
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package org.example.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.logging.Logger;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+    private static final Logger logger = Logger.getLogger(JenkinsDemoApplicationTests.class.getName());
+
+    @Test
+    void contextLoads() {
+        logger.info("tests executing");
+        assert true;
+    }
+
+}
