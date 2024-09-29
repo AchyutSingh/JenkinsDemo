bda4ed2b8e3e05fffdbf3353d79dab052c4c81d7
AchyutSingh <achyut.singh@reltio.com>
2024-09-29 13:07:23 +0530

    added jar file name and server port

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:17
 EXPOSE 8080
-ADD target/JenkinsDemo-application.jar JenkinsDemo-application.jar
-ENTRYPOINT ["java","-jar","/JenkinsDemo-application.jar"]
+ADD target/jenkins-demo-application.jar jenkins-demo-application.jar
+ENTRYPOINT ["java","-jar","/jenkins-demo-application.jar"]
--- src/main/java/org/example/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/org/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -3,16 +3,27 @@ package org.example.jenkinsdemo;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Logger;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class JenkinsDemoApplication {
     private static final Logger logger = Logger.getLogger(JenkinsDemoApplication.class.getName());
 
     @PostConstruct
     public void init(){
         logger.info("init Application RP");
+    }
+
+    @GetMapping("/get")
+    public String getTest(){
+        logger.info("getTest");
+        return "get test API";
     }
 
     public static void main(String[] args) {
